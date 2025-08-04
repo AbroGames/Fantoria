042ea01c4e9db8cc53694cc9871cc6c930d99187
Evgeniy Serov <kejik148@gmail.com>
2025-08-04 22:38:22 +0500

    Add camera utils to lib

---
+++ Lib/Nodes/Camera/Shifts/ManualShake.cs
@@ -0,0 +1,21 @@
+﻿using Fantoria.Lib.Services;
+using Godot;
+
+namespace Fantoria.Lib.Nodes.Camera.Shifts;
+
+public class ManualShake : IShiftProvider
+{
+    /// <inheritdoc />
+    public Vector2 Shift => IsAlive ? LibService.Rand.InsideUnitCircle * Strength : Vec();
+
+    public float Strength { get; set; } = 0;
+
+    /// <inheritdoc />
+    public bool IsAlive { get; set; } = true;
+
+    /// <inheritdoc />
+    public void Update(double delta)
+    {
+        // do nothing
+    }
+}
