60e671523e52c877a55913c5d7c8d13e6c46685d
Evgeniy Serov <kejik148@gmail.com>
2025-08-05 22:47:01 +0500

    Add Extensions

---
+++ Lib/Utils/Extensions/RandExtensions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Fantoria.Lib.Services;
+
+namespace Fantoria.Lib.Utils.Extensions;
+
+public static class RandExtensions
+{
+    /// <summary>
+    /// Возвращает случайный элемент списка
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    public static T GetRandom<T>(this IEnumerable<T> sequence)
+    {
+        if (sequence == null)
+        {
+            throw new ArgumentNullException();
+        }
+
+        if (!sequence.Any())
+        {
+            throw new ArgumentException("The sequence is empty.");
+        }
+
+        //optimization for ICollection<T>
+        if (sequence is ICollection<T>)
+        {
+            ICollection<T> col = (ICollection<T>)sequence;
+            return col.ElementAt(LibService.Rand.Range(col.Count));
+        }
+
+        int count = 1;
+        T selected = default(T);
+
+        foreach (T element in sequence)
+        {
+            if (LibService.Rand.Range(count++) == 0)
+            {
+                //Select the current element with 1/count probability
+                selected = element;
+            }
+        }
+
+        return selected;
+    }
+}
---
+++ Lib/Utils/Extensions/RectExtensions.cs
@@ -0,0 +1,60 @@
+using Godot;
+
+namespace Fantoria.Lib.Utils.Extensions;
+
+public static class RectExtensions
+{
+    public static Vector2[] ToPolygon(this Rect2 rect)
+    {
+        return [
+            rect.Position, // top left
+            rect.Position + Vec(rect.Size.X, 0), // top right
+            rect.End, // bottom right
+            rect.End - Vec(rect.Size.X, 0) // bottom left
+        ];
+    }
+    
+    
+    public static Vector2I[] ToPolygon(this Rect2I rect)
+    {
+        return [
+            rect.Position, // top left
+            rect.Position + VecI(rect.Size.X, 0), // top right
+            rect.End, // bottom right
+            rect.End - VecI(rect.Size.X, 0) // bottom left
+        ];
+    }
+    
+    /// <summary>
+    /// Gets the X-coordinate of the top-left corner of the specified Rect2.
+    /// </summary>
+    /// <param name="rect">The Rect2 object.</param>
+    /// <returns>The X-coordinate of the top-left corner.</returns>
+    public static float X(this Rect2 rect) => rect.Position.X;
+
+    /// <summary>
+    /// Gets the Y-coordinate of the top-left corner of the specified Rect2.
+    /// </summary>
+    /// <param name="rect">The Rect2 object.</param>
+    /// <returns>The Y-coordinate of the top-left corner.</returns>
+    public static float Y(this Rect2 rect) => rect.Position.Y;
+
+    /// <summary>
+    /// Gets the width of the specified Rect2.
+    /// </summary>
+    /// <param name="rect">The Rect2 object.</param>
+    /// <returns>The width of the Rect2.</returns>
+    public static float Width(this Rect2 rect) => rect.Size.X;
+
+    /// <summary>
+    /// Gets the height of the specified Rect2.
+    /// </summary>
+    /// <param name="rect">The Rect2 object.</param>
+    /// <returns>The height of the Rect2.</returns>
+    public static float Height(this Rect2 rect) => rect.Size.Y;
+
+    public static float GetCircumradius(this Rect2 rect)
+    {
+        return Mathf.Sqrt(rect.Width() * rect.Width() + rect.Height() * rect.Height()) / 2;
+    }
+}
