9b0dab253c3e6ec683a9fa50388de745ef645d14
Evgeniy Serov <kejik148@gmail.com>
2025-08-06 22:50:33 +0500

    Add NodeContainer util

---
+++ Lib/Nodes/Container/NodeContainer.cs
@@ -0,0 +1,40 @@
+using System;
+using Godot;
+
+namespace Fantoria.Lib.Nodes.Container;
+
+// This script must be added to node in Godot editor
+// This script can be added to node with any type, not only type "Node"
+public partial class NodeContainer : Node
+{
+    private Node _currentStoredNode;
+
+    public override void _Ready()
+    {
+        if (GetChildCount() > 1) throw new InvalidOperationException($"NodeContainer must has not more 1 child. Has '{GetChildCount()}' children.");
+        if (GetChildCount() == 1)
+        {
+            _currentStoredNode?.QueueFree();
+            _currentStoredNode = GetChildren()[0];
+        }
+    }
+
+    public Node ChangeStoredNode(Node newStoredNode)
+    {
+        _currentStoredNode?.QueueFree();
+        _currentStoredNode = newStoredNode;
+        AddChild(newStoredNode);
+        return newStoredNode;
+    }
+
+    public void ClearStoredNode()
+    {
+        _currentStoredNode?.QueueFree();
+        _currentStoredNode = null;
+    }
+
+    public TStored GetCurrentStoredNode<TStored>() where TStored : Node
+    {
+        return _currentStoredNode as TStored;
+    }
+}
