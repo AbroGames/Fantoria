6c6f9492302bd9c3c5aba69beec6198fd30956e5
Evgeniy Serov <kejik148@gmail.com>
2025-08-07 22:54:10 +0500

    Add Lib global using util

---
+++ Lib/GlobalUsings.cs
@@ -0,0 +1,15 @@
+﻿global using static Fantoria.Lib.Utils.Extensions.CameraExtensions;
+global using static Fantoria.Lib.Utils.Extensions.ColorExtensions;
+global using static Fantoria.Lib.Utils.Extensions.MathExtensions;
+global using static Fantoria.Lib.Utils.Extensions.Node2DExtensions;
+global using static Fantoria.Lib.Utils.Extensions.NodeNetworkExtensions;
+global using static Fantoria.Lib.Utils.Extensions.NodeTreeExtensions;
+global using static Fantoria.Lib.Utils.Extensions.RandExtensions;
+global using static Fantoria.Lib.Utils.Extensions.RectExtensions;
+global using static Fantoria.Lib.Utils.Extensions.SpriteExtensions;
+global using static Fantoria.Lib.Utils.Extensions.VectorExtensions;
+
+global using Fantoria.Lib.Services.NotNullChecking;
+
+global using Fantoria.Lib.Services;
+global using static Fantoria.Lib.Services.LibService.Global;
