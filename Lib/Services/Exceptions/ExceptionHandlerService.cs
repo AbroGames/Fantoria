7d05e0db4525cef3c458c5a2b3765b7febdfc1c8
Evgeniy Serov <kejik148@gmail.com>
2025-08-07 22:52:34 +0500

    Add service for exception handling

---
+++ Lib/Services/Exceptions/ExceptionHandlerService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+using Godot;
+
+namespace Fantoria.Lib.Services.Exceptions;
+
+[Service]
+public class ExceptionHandlerService
+{
+    public void AddExceptionHandlerForUnhandledException()
+    {
+        AppDomain.CurrentDomain.UnhandledException += HandleException;
+    }
+    
+    private void HandleException(object sender, UnhandledExceptionEventArgs args)
+    {
+        Log.Info();
+        // If logging will produce unhandled exception then we fucked up, so we need try/catch here.
+        try
+        {
+            if (args.ExceptionObject is not Exception exception) return;
+			
+            Log.Error(exception.ToString());
+        }
+        catch (Exception e)
+        {
+            // Use GD.Print instead of Log.Error to avoid infinite recursion.
+            GD.Print($"Unexpected exception was thrown while handling unhandled exception: {e}");
+        }
+		
+        Debugger.Break();
+    }
+}
