db4c30e7342020e123a6fd59eacb15cd215a1eee
Evgeniy Serov <kejik148@gmail.com>
2025-08-07 22:54:23 +0500

    Add lib initializer

---
+++ Lib/LibInitializer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Fantoria.Lib.Utils.Extensions;
+using Godot;
+
+namespace Fantoria.Lib;
+
+public class LibInitializer
+{
+
+    private Func<Node, bool> _nodeNetworkExtensionsIsClientChecker;
+
+    public LibInitializer SetNodeNetworkExtensionsIsClientChecker(Func<Node, bool> isClientChecker)
+    {
+        _nodeNetworkExtensionsIsClientChecker = isClientChecker;
+        return this;
+    }
+    
+    public void Init()
+    {
+        if (_nodeNetworkExtensionsIsClientChecker == null)
+        {
+            throw new Exception("You must set NodeNetworkExtensionsIsClientChecker before calling Init function");
+        }
+        
+        ServiceLocator.Init();
+        Log.Info("Lib initializing...");
+        
+        LibService.ExceptionHandler.AddExceptionHandlerForUnhandledException();
+        LibService.CmdArgs.LogCmdArgs();
+        NodeNetworkExtensionsState.SetIsClientChecker(_nodeNetworkExtensionsIsClientChecker);
+    }
+}
