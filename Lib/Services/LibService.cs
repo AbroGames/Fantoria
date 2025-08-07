7a8eaf1fe205d6df4b94bd45f6ed4b2bdeb7011c
Evgeniy Serov <kejik148@gmail.com>
2025-08-07 22:53:56 +0500

    Add static Service handler to lib

---
+++ Lib/Services/LibService.cs
@@ -0,0 +1,22 @@
+﻿using Fantoria.Lib.Services.CmdArgs;
+using Fantoria.Lib.Services.Exceptions;
+using Fantoria.Lib.Services.Logging;
+using Fantoria.Lib.Services.Maths;
+using Fantoria.Lib.Services.NotNullChecking;
+
+namespace Fantoria.Lib.Services;
+
+public static class LibService
+{ 
+    public static ExceptionHandlerService ExceptionHandler => ServiceLocator.Get<ExceptionHandlerService>();
+    public static CmdArgsService CmdArgs => ServiceLocator.Get<CmdArgsService>();
+    public static LogService Log => ServiceLocator.Get<LogService>();
+    public static NotNullCheckerService NotNullChecker => ServiceLocator.Get<NotNullCheckerService>();
+    public static RandomService Rand => ServiceLocator.Get<RandomService>();
+    
+    public static class Global
+    {
+        public static LogService Log => LibService.Log; 
+        public static NotNullCheckerService NotNullChecker => LibService.NotNullChecker;
+    }
+}
