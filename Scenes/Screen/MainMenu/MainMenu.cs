9b5666cfcee6be0afcf1e4dbe8255f24e506781e
Evgeniy Serov <kejik148@gmail.com>
2025-08-08 22:56:21 +0500

    Add MainMenu scene

---
+++ Scenes/Screen/MainMenu/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using Fantoria.Lib.Nodes.Container;
+using Godot;
+
+namespace Fantoria.Scenes.Screen.MainMenu;
+
+public partial class MainMenu : Node2D
+{
+    [Export] [NotNull] public NodeContainer BackgroundContainer { get; private set; }
+    [Export] [NotNull] public NodeContainer MenuContainer { get; private set; }
+	
+    public override void _Ready()
+    {
+        NotNullChecker.CheckProperties(this);
+    }
+    
+    //TODO в сервис? Но по сути тогда снова засинглтоним меню. В сервисы можно выносить только сцены/вещи существующие в любой момент времени игры (и в меню и в игре).
+    //TODO Этот объект по сути эквивалент объекта Game
+    public Node ChangeMenu(PackedScene newMenu)  
+    {
+        return MenuContainer.ChangeStoredNode(newMenu.Instantiate());
+    }
+}
