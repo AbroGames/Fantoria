eca212da48e7a633d02759638df85ed661065ab5
Evgeniy Serov <kejik148@gmail.com>
2025-08-08 22:55:18 +0500

    Add LoadingScreen service

---
+++ Scripts/Services/LoadingScreen/LoadingScreenService.cs
@@ -0,0 +1,42 @@
+﻿using Fantoria.Lib.Nodes.Container;
+using Fantoria.Scripts.Content.LoadingScreen;
+using LoadingScreenNode = Fantoria.Scenes.Screen.LoadingScreen.LoadingScreen;
+using Godot;
+
+namespace Fantoria.Scripts.Services.LoadingScreen;
+
+[Service]
+public class LoadingScreenService
+{
+    
+    private NodeContainer _loadingScreenContainer;
+    private PackedScene _loadingScreenPackedScene;
+
+    public void Init(NodeContainer loadingScreenContainer, PackedScene loadingScreenPackedScene)
+    {
+        _loadingScreenContainer = loadingScreenContainer;
+        _loadingScreenPackedScene = loadingScreenPackedScene;
+    }
+    
+    public LoadingScreenNode SetLoadingScreen(string text)
+    {
+        LoadingScreenNode loadingScreen = _loadingScreenPackedScene.Instantiate<LoadingScreenNode>();
+        if (text != null)
+        {
+            loadingScreen.SetText(text);
+        }
+
+        _loadingScreenContainer.ChangeStoredNode(loadingScreen);
+        return loadingScreen;
+    }
+    
+    public LoadingScreenNode SetLoadingScreen(LoadingScreenTypes.Type loadingScreenType)
+    {
+        return SetLoadingScreen(LoadingScreenTypes.GetLoadingScreenText(loadingScreenType));
+    }
+	
+    public void Clear()
+    {
+        _loadingScreenContainer.ClearStoredNode();
+    }
+}
