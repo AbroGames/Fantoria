698539f6d65998f56b61caea1291e161d4d2196a
Evgeniy Serov <kejik148@gmail.com>
2025-08-08 22:54:48 +0500

    Add service handler to game

---
+++ Scripts/Services/Service.cs
@@ -0,0 +1,19 @@
+﻿using Fantoria.Scripts.Services.CmdArgs;
+using Fantoria.Scripts.Services.LoadingScreen;
+using Fantoria.Scripts.Services.MainScene;
+using Fantoria.Scripts.Services.Process;
+
+namespace Fantoria.Scripts.Services;
+
+public static class Service
+{
+    public static CmdArgsService CmdArgs => ServiceLocator.Get<CmdArgsService>();
+    public static ProcessService Process => ServiceLocator.Get<ProcessService>();
+    public static LoadingScreenService LoadingScreen => ServiceLocator.Get<LoadingScreenService>();
+    public static MainSceneService MainScene => ServiceLocator.Get<MainSceneService>();
+
+    public static class Global
+    {
+        
+    }
+}
