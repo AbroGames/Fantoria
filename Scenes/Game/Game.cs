f97e8323fa1d305bb7ee86bdb4d3831827049987
Evgeniy Serov <kejik148@gmail.com>
2025-08-09 22:57:44 +0500

    Add game node

---
+++ Scenes/Game/Game.cs
@@ -0,0 +1,30 @@
+﻿using Fantoria.Scenes.Game.Starters;
+using Godot;
+
+namespace Fantoria.Scenes.Game;
+
+public partial class Game : Node2D
+{
+    
+    public Network Network { get; private set; } //TODO readonly?
+
+    public Game Init(BaseGameStarter gameStarter)
+    {
+        gameStarter.Init(this);
+        return this;
+    }
+}
+
+
+//TODO перенести аккуратно картинки и сцену из NeonWarfare с LoadingScreen
+        
+//TODO какая структура папок Screen, учитывая что половина игры -- это меню? Вторая половина -- бои. Внутри Game снова разделение на два типа?
+//TODO учитывая сеть нужна возможность "сворачивать" бой и возвращаться к обычной игре, и боев может быть несколько одновременно. И действовать там могут все игроки (по дефолту).
+        
+//TODO Потребуются GameState и машина состояний? Как в Neon, но полноценная? Или разделить типа на бек и фронт, и все валидировать на "беке"?
+//TODO Возможность загрузки игры из файла сейва из какого-то состояния? По сути это один-в-один логика как синк с сервером.
+//TODO PingService / PingNode стырить из Neon сюда в Lib, без сетевой логики, чисто как аналитика и record пакета
+        
+//TODO В каждой ноде один раз получать в _Ready текущий Game через сервис? Или через родителей? А все остальные GameServices вложены в Game?
+
+//TODO Чекнуть все ошибки it can be readonly (создать такую искусственно, а потом поискать по всем)
---
+++ Scenes/Game/Network.cs
@@ -0,0 +1,15 @@
+﻿namespace Fantoria.Scenes.Game;
+
+public class Network
+{
+
+    public void ConnectToServer(string host, int port)
+    {
+        
+    }
+    
+    public void HostServer(int port)
+    {
+        
+    }
+}
