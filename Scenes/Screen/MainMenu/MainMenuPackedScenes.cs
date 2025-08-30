6b5c48d75af73988da27483423cf025bfdbf9d44
Evgeniy Serov <kejik148@gmail.com>
2025-08-30 19:48:00 +0500

    Create MainMunePage and PackedScenes

---
+++ Scenes/Screen/MainMenu/MainMenuPackedScenes.cs
@@ -0,0 +1,16 @@
+﻿using Godot;
+
+namespace Fantoria.Scenes.Screen.MainMenu;
+
+public partial class MainMenuPackedScenes : Node
+{
+    
+    [Export] [NotNull] public PackedScene Main { get; private set; }
+    [Export] [NotNull] public PackedScene CreateServer { get; private set; }
+    [Export] [NotNull] public PackedScene ConnectToServer { get; private set; }
+    
+    public override void _Ready()
+    {
+        NotNullChecker.CheckProperties(this);
+    }
+}
