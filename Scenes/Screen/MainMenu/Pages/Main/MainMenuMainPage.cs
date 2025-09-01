bad2cc7593598c2476e27f3064e0ad2325d3390e
Evgeniy Serov <kejik148@gmail.com>
2025-09-01 19:48:29 +0500

    Add MainPage in menu

---
+++ Scenes/Screen/MainMenu/Pages/Main/MainMenuMainPage.cs
@@ -0,0 +1,21 @@
+using Fantoria.Scenes.Game.Starters;
+using Godot;
+
+namespace Fantoria.Scenes.Screen.MainMenu.Pages.Main;
+
+public partial class MainMenuMainPage : MainMenuPage
+{
+    
+    [Export] [NotNull] public Button StartSingleplayerButton { get; private set; }
+    [Export] [NotNull] public Button CreateServerButton { get; private set; }
+    [Export] [NotNull] public Button ConnectToServerButton { get; private set; }
+    
+    public override void _Ready()
+    {
+        NotNullChecker.CheckProperties(this);
+        
+        StartSingleplayerButton.Pressed += () => Service.MainScene.StartSingleplayerGame();
+        CreateServerButton.Pressed += () => ChangeMenuPage(PackedScenes.CreateServer);
+        ConnectToServerButton.Pressed += () => ChangeMenuPage(PackedScenes.ConnectToServer);
+    }
+}
