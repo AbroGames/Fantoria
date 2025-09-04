6e2681e105c6bc11868a99335e44767645a5379e
Evgeniy Serov <kejik148@gmail.com>
2025-09-04 21:48:01 +0500

    Add Hud scene

---
+++ Scenes/Screen/Hud/Hud.cs
@@ -0,0 +1,15 @@
+﻿using Godot;
+
+namespace Fantoria.Scenes.Screen.Hud;
+
+public partial class Hud : Control
+{
+    
+    private World.World _world;
+    
+    public void Init(World.World world)
+    {
+        if (world == null) Log.Error("World must be not null");
+        _world = world;
+    }
+}
