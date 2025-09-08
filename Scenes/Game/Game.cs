b7971982adb962700da2fd8e255cbae7770f15e9
Evgeniy Serov <kejik148@gmail.com>
2025-09-08 12:52:50 +0500

    Refactoring game, add WorldPackedScenes

--- Scenes/Game/Game.cs
+++ Scenes/Game/Game.cs
@@ -2,6 +2,7 @@
 using Fantoria.Scenes.Game.Net;
 using Fantoria.Scenes.Game.Starters;
 using Fantoria.Scenes.Screen.Hud;
+using Fantoria.Scenes.World;
 using Godot;
 
 namespace Fantoria.Scenes.Game;
@@ -12,6 +13,10 @@ public partial class Game : Node2D
     [Export] [NotNull] public NodeContainer WorldContainer { get; set; }
     [Export] [NotNull] public NodeContainer HudContainer { get; set; }
     [Export] [NotNull] public GamePackedScenes PackedScenes { get; set; }
+
+    private Network _network;
+    private Synchronizer _synchronizer;
+    private WorldMultiplayerSpawner _worldMultiplayerSpawner;
 
     public override void _Ready()
     {
@@ -28,28 +33,36 @@ public partial class Game : Node2D
         World.World world = PackedScenes.World.Instantiate<World.World>();
         world.SetName("World");
         WorldContainer.ChangeStoredNode(world);
+        
+        _worldMultiplayerSpawner?.QueueFree();
+        _worldMultiplayerSpawner = world.WorldMultiplayerSpawnerPackedScene.Instantiate<WorldMultiplayerSpawner>()
+            .Init(world);
+        this.AddChildWithName(_worldMultiplayerSpawner, "MultiplayerSpawner");
+        
         return world;
     }
     
     public Hud AddHud()
     {
-        Hud hud = PackedScenes.Hud.Instantiate<Hud>();
-        hud.Init(WorldContainer.GetCurrentStoredNode<World.World>());
+        Hud hud = PackedScenes.Hud.Instantiate<Hud>()
+            .Init(WorldContainer.GetCurrentStoredNode<World.World>());
         HudContainer.ChangeStoredNode(hud);
         return hud;
     }
 
     public Network AddNetwork()
     {
-        Network network = new Network();
-        this.AddChildWithName(network, "Network");
-        return network;
+        _network?.QueueFree();
+        _network = new Network();
+        this.AddChildWithName(_network, "Network");
+        return _network;
     }
 
     public Synchronizer AddSynchronizer()
     {
-        Synchronizer synchronizer = new Synchronizer(this);
-        this.AddChildWithName(synchronizer, "Synchronizer");
-        return synchronizer;
+        _synchronizer?.QueueFree();
+        _synchronizer = new Synchronizer();
+        this.AddChildWithName(_synchronizer, "Synchronizer");
+        return _synchronizer;
     }
 }
