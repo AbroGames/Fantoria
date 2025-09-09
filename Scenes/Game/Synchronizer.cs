c893322a213743f276e810592363beb4471488df
Evgeniy Serov <kejik148@gmail.com>
2025-09-09 12:53:29 +0500

    Fix Synchronizer

--- Scenes/Game/Synchronizer.cs
+++ Scenes/Game/Synchronizer.cs
@@ -3,32 +3,32 @@ using Godot;
 
 namespace Fantoria.Scenes.Game;
 
-public partial class Synchronizer(Game game) : Node
+//TODO Мб перенести класс в World, т.к. данные игрока туда синкаются, а LoadingScreen можно дергать через назначаемые Action-ы
+//TODO Но синхронайзер удобен тут тем, что он по разному вызывается из стартеров. Мб часть оставить тут, но отсюда просто вызывать Action-ы в World? В World.PlayerData?
+//TODO Оставить тут логику скринов, или это в Hud? По идее Hud привязан к World только должен быть, и LoadingScreen -- это другой объект, а не Hud
+public partial class Synchronizer : Node
 {
 
-    public void ConnectedToServerEvent()
+    public void StartSyncOnClient()
     {
         Service.LoadingScreen.SetLoadingScreen(LoadingScreenTypes.Type.Loading);
         //TODO Take player info from Game/Settings
         NewClientInitOnServer();
     }
 
-    private void NewClientInitOnServer() => Rpc(MethodName.NewClientInitOnServerRpc);
-    [Rpc(MultiplayerApi.RpcMode.AnyPeer, TransferMode = MultiplayerPeer.TransferModeEnum.Reliable)]
+    private void NewClientInitOnServer() => RpcId(ServerId, MethodName.NewClientInitOnServerRpc);
+    [Rpc(MultiplayerApi.RpcMode.AnyPeer, CallLocal = true)] 
     private void NewClientInitOnServerRpc()
     {
         //TODO Receive and store nickname, color and etc
-        //TODO Resend client info to another clients. Use this method?
+        //TODO Resend client info to another clients by MpSync in World
         SyncWorldDataOnClient(GetMultiplayer().GetRemoteSenderId());
     }
 
     private void SyncWorldDataOnClient(int id) => RpcId(id, MethodName.SyncWorldDataOnClientRpc);
-    [Rpc(TransferMode = MultiplayerPeer.TransferModeEnum.Reliable)]
+    [Rpc(CallLocal = true)] 
     private void SyncWorldDataOnClientRpc()
     {
-        game.AddWorld();
-        game.AddHud();
-        //TODO Receive world state as gzip json byte array from server SaveLoad system, and load it into client SaveLoad system
         Service.LoadingScreen.Clear();
     }
 }
