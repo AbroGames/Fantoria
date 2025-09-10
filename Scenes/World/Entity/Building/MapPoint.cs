152785cfbcb987dea24ca06d0ed4f657db6b1126
Evgeniy Serov <kejik148@gmail.com>
2025-09-10 15:06:46 +0500

    Refactoring MapPoint

--- Scenes/World/Entity/Building/MapPoint.cs
+++ Scenes/World/Entity/Building/MapPoint.cs
@@ -4,14 +4,5 @@ namespace Fantoria.Scenes.World.Entity.Building;
 
 public partial class MapPoint : Node2D
 {
-
-    [Export] public int a { get; set; } //TODO del
-    [Export] protected int b;
-    [Export] public int c;
     
-    public override void _Ready()
-    {
-        Log.Warning("Position: " + Position + " ; " + c);
-        if (this.IsServer()) c = 10;
-    }
 }
