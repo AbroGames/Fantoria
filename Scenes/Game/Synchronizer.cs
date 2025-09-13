f5db2ea9464c59baeda8e146d18c46e9ff49678f
Evgeniy Serov <kejik148@gmail.com>
2025-09-13 16:47:02 +0500

    Add Color to PlayerData and sync it

--- Scenes/Game/Synchronizer.cs
+++ Scenes/Game/Synchronizer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Fantoria.Scenes.World.Data.Player;
 using Godot;
 
 namespace Fantoria.Scenes.Game;
@@ -28,12 +29,13 @@ public partial class Synchronizer : Node
     {
         SyncStartedOnClientEvent.Invoke();
         string nick = "TestNick-" + Random.Shared.Next(); //TODO Take player info from Game/Settings
-        NewClientInitOnServer(nick);
+        Color color = new Color(1, 1, 1);
+        NewClientInitOnServer(nick, color);
     }
 
-    private void NewClientInitOnServer(string nick) => RpcId(ServerId, MethodName.NewClientInitOnServerRpc, nick);
+    private void NewClientInitOnServer(string nick, Color color) => RpcId(ServerId, MethodName.NewClientInitOnServerRpc, nick, color);
     [Rpc(MultiplayerApi.RpcMode.AnyPeer, CallLocal = true)] 
-    private void NewClientInitOnServerRpc(string nick)
+    private void NewClientInitOnServerRpc(string nick, Color color)
     {
         //TODO Receive and store nickname, color and etc in World.PlayerData. Client info automatically resends to another clients by MpSync.
         int connectedClientId = GetMultiplayer().GetRemoteSenderId();
@@ -42,7 +44,17 @@ public partial class Synchronizer : Node
         {
             RejectSyncOnClient(connectedClientId, "Nickname is already used");
         }
-        _world.PlayerNickByPeerId.Add(connectedClientId, nick); 
+        _world.PlayerNickByPeerId.Add(connectedClientId, nick);
+
+        if (!_world.Data.Players.PlayerByNick.ContainsKey(nick))
+        {
+            PlayerData playerData = new()
+            {
+                Nick = nick,
+                Color = color
+            };
+            _world.Data.Players.AddPlayer(playerData);
+        }
         
         EndSyncOnClient(connectedClientId, _world.Data.Serialize());
     }
--- Scenes/World/Data/Player/PlayerData.cs
+++ Scenes/World/Data/Player/PlayerData.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Godot;
 using MessagePack;
 
 namespace Fantoria.Scenes.World.Data.Player;
@@ -9,4 +10,19 @@ public partial class PlayerData : ObservableObject
     
     [Key(0)] [ObservableProperty] private string _nick;
     
+    [Key(1)] [ObservableProperty] private float _colorR;
+    [Key(2)] [ObservableProperty] private float _colorG;
+    [Key(3)] [ObservableProperty] private float _colorB;
+
+    [IgnoreMember]
+    public Color Color
+    {
+        get => new(ColorR, ColorG, ColorB);
+        set
+        {
+            ColorR = value.R;
+            ColorG = value.G;
+            ColorB = value.B;
+        }
+    } 
 }
